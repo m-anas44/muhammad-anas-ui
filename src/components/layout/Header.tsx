035ac3e2d7c9bc7f157cb63075b112cf567af4dc
m-anas44 <125805038+m-anas44@users.noreply.github.com>
2023-08-02 15:24:04 +0500

    new update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,48 +1,69 @@
-import Image from "next/image";
-import Link from "next/link";
-import image from "public/logo_white.png";
-import { ShoppingCart } from "lucide-react";
-import { SearchIcon } from "lucide-react";
-import { Input } from "../ui/input";
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '/public/logo_white.png'
+import { ShoppingCart, Search, MenuIcon, XIcon } from "lucide-react";
+import { Input } from '../ui/input';
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="flex flex-col lg:flex-row md:flex-row gap-y-3 justify-between items-center
-     px-8 lg:px-12 py-5 text-white bg-gray-900 shadow-lg sticky top-0 z-50">
-      <Link href={"/"} className="flex flex-row items-center select-none">
-        <Image src={image} alt="logo" className="w-10" />
+    <nav className='flex justify-between items-center flex-wrap text-white bg-gray-900 shadow-lg sticky top-0 
+    z-50 px-8 lg:px-12 py-4'>
+
+      <Link href={"/"} className="flex flex-row items-center select-none lg:mr-60">
+        <Image src={logo} alt="logo" className="w-10" />
         <p className="text-3xl font-bold text-white">&nbsp;MARKET</p>
       </Link>
-      <ul className="flex gap-x-6 text-lg flex-wrap justify-center">
-        <li>
-          <Link href={"/categories/female"}>Female</Link>
-        </li>
-        <li>
-          <Link href={"/categories/male"}>Male</Link>
-        </li>
-        <li>
-          <Link href={"/categories/kids"}>Kids</Link>
-        </li>
-        <li>
-          <Link href={"/categories/sports"}>Sports</Link>
-        </li>
-        <li>
-          <Link href={"/products"}>All</Link>
-        </li>
-      </ul>
-      <div className=" hidden lg:flex flex-row items-center">
-        <Input className="w-60 rounded-l-full border" placeholder=" Search" />
-        <button
-          type="button"
-          name="Search"
-          className="border-white border  p-[7px] border-l-0 bg-white rounded-r-full">
-          <SearchIcon className="text-gray-900 " />
+      <div className='block lg:hidden'>
+        <button type="button" onClick={() => { setIsOpen(!isOpen) }} className='border p-1 rounded-lg border-gray-300'>
+          <MenuIcon className={`fill-current ${isOpen ? "hidden" : "block"}`} />          
+          <XIcon className={`fill-current ${isOpen ? "block" : "hidden"}`} />          
         </button>
       </div>
-      <div className="w-10 h-10 rounded-full bg-white lg:flex justify-center items-center 
-      hidden lg:visible">
-        <ShoppingCart className="text-gray-900" />
+      {/* LINKS to show and hide font-bold text-red-500  */}
+      <div className={`block flex-grow justify-between lg:flex lg:items-center 
+      lg:w-auto w-full text-white
+       ${isOpen ? "flex flex-col-reverse bg-gray-700 opacity-80 rounded-lg mt-5" : "hidden"}`}>
+
+        <ul className="text-sm ml-3 lg:ml-0 lg:flex gap-x-4 gap-y-3">
+          <li>
+            <Link href={"/categories/female"} className='block lg:inline-block text-lg p-3'>Female</Link>
+          </li>
+          <li>
+            <Link href={"/categories/male"} className='block lg:inline-block text-lg p-3'>Male</Link>
+          </li>
+          <li>
+            <Link href={"/categories/kids"} className='block lg:inline-block text-lg p-3'>Kids</Link>
+          </li>
+          <li>
+            <Link href={"/categories/sports"} className='block lg:inline-block text-lg p-3'>Sports</Link>
+          </li>
+          <li>
+            <Link href={"/products"} className='block lg:inline-block text-lg p-3'>All</Link>
+          </li>
+        </ul>
+
+        {/* search baar to show */}
+
+        <div className={`lg:flex gap-x-3 ${isOpen ? "flex p-[12px] justify-between w-full" : "hidden"}`}>
+          <div className="flex flex-row items-center w-full">
+            <Input className="w-full border-gray-400 border-[1px] shadow-none rounded-l-full text-gray-400 lg:border p-[6px]" placeholder=" Search" />
+            <button
+              type="button"
+              name="Search"
+              className="border-gray-400 border-2 p-[6px] border-l-0 rounded-r-full">
+              <Search className="text-gray-400" />
+            </button>
+          </div>
+          <div className="w-16 h-10 flex rounded-full bg-white justify-center items-center">
+            <ShoppingCart className="text-gray-900 text-2xl" />
+          </div>
+        </div>
+
+
       </div>
-    </div>
+    </nav>
   );
 };
 export default Header;
